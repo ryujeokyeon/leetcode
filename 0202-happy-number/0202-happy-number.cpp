812a14bd8e0ee178cdb30ae44ba67a5fc21c0709
Annisa <114063803+ryujeokyeon@users.noreply.github.com>
2023-09-24 06:57:42 +0700

    Time: 3 ms (43.17%), Space: 6.3 MB (34.57%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -12,11 +12,16 @@ int sum_of_squares(int num){
     bool isHappy(int n) {
      if(n <= 0)
      return false;
-     unordered_set<int> cache;
-     while(cache.find(n) == cache.end()){
-         cache.insert(n);
-         n = sum_of_squares(n);
-         if(n == 1)
+
+     int slow_sum = n;
+     int fast_sum = sum_of_squares(n);
+     if (slow_sum == 1 || fast_sum == 1)
+     return true;
+     
+     while(slow_sum != fast_sum){
+     slow_sum = sum_of_squares(slow_sum);
+     fast_sum = sum_of_squares(sum_of_squares(fast_sum));
+     if(slow_sum == 1 || fast_sum == 1)
          return true;
      }   
      return false;
